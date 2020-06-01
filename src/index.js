12a784d46ee1e40b99a208dda9fcde3c1b717024
luigi <luigilamastra@gmail.com>
2020-06-01 13:10:33 +0200

    8 - Dispatching Actions

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log("Hello World!");
+console.log('funzia')
+
+import store from './store'
+
+console.log(store);
+store.dispatch({type:'bugAdded', payload:{description: 'bug1'}})
+store.dispatch({type:'bugAdded', payload:{description: 'bug2'}})
+store.dispatch({type: 'bugRemoved', payload:{id: 1}})
+console.log(store.getState());
