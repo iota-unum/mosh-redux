2b902d6a30ae54d0b51143db0d6d2fbcc3907ae8
luigi <luigilamastra@gmail.com>
2020-06-01 13:48:23 +0200

    9 - Subscribing to the Store

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ console.log('funzia')
 
 import store from './store'
 
-console.log(store);
+const unsubscribe = store.subscribe(()=>{
+    console.log('Store Changed!!!');
+    
+})
 store.dispatch({type:'bugAdded', payload:{description: 'bug1'}})
 store.dispatch({type:'bugAdded', payload:{description: 'bug2'}})
+unsubscribe()
 store.dispatch({type: 'bugRemoved', payload:{id: 1}})
 console.log(store.getState());
