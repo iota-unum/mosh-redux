5f352dbd49b90369d62b1306469f108cde3d563e
luigi <luigilamastra@gmail.com>
2020-06-06 14:34:43 +0200

    brfs Subscribing to the Store

--- src/customStore.js
+++ src/customStore.js
@@ -1,10 +1,22 @@
 import reducer from './reducer'
 const createStore = () => {
   let state
+  let listeners = []
   const getState = () => state
-  const subscribe = () => {}
+  const subscribe = (listener) => {
+   
+    listeners.push(listener)
+    return () => {
+      console.log('UNSUBSCRIBED!');
+      
+      listeners = listeners.filter(lis => lis !== listener)
+    }
+  }
   const dispatch = (action) => {
     state = reducer(state, action)
+    for(let i = 0; i < listeners.length; i++) {
+      listeners[i]()
+    }
   }
   return {
     getState,
--- src/index.js
+++ src/index.js
@@ -1,21 +1,16 @@
-console.log('funzia')
 import {bugAdded , bugRemoved, bugResolved} from './actions'
 import store from './customStore'
 
-console.log(store)
 console.log(store.getState());
 
 
-// const unsubscribe = store.subscribe(()=>{
-//     console.log('Store Changed!!!');
+ const unsubscribe = store.subscribe(()=>{
+    console.log('Store Changed!!!');
+    console.log('STORE',store.getState());
     
-// })
-console.log('0',store.getState());
+})
 store.dispatch(bugAdded('Bug1'))
-console.log('1',store.getState());
 store.dispatch(bugAdded('Bug2'))
-console.log('2',store.getState());
+unsubscribe()
 store.dispatch(bugResolved(2))
-// unsubscribe()
 store.dispatch(bugRemoved(1))
-console.log('3',store.getState());
