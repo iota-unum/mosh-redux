323c65f533b1ac7b6efdae23e265119144bbb6e2
luigi <luigilamastra@gmail.com>
2020-06-07 12:37:54 +0200

    wcrc Creating the Store

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,14 +1,6 @@
-import { createStore } from 'redux'
-import {devToolsEnhancer} from 'redux-devtools-extension'
+import {configureStore} from '@reduxjs/toolkit'
 import reducer from './bugs'
 
-const store = createStore(
-  reducer,
- devToolsEnhancer({trace: true})
-)
-
-const configureStore = () => {
-  return store
+export default function() {
+  return configureStore({reducer})
 }
-
-export default configureStore
