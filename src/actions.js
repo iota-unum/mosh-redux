5cd834595420c5ceb06d9dc6f620c5a943b856fc
luigi <luigilamastra@gmail.com>
2020-06-07 12:09:54 +0200

    Implemented ducks pattern except for the store

--- src/actions.js
+++
@@ -1,8 +0,0 @@
-import * as actions from './actionTypes'
-
-
-export const bugAdded = description => ({type:actions.BUG_ADDED, payload:{description: description}})
-
-export const bugRemoved = id => ({type: 'bugRemoved', payload:{id: id}})
-
-export const bugResolved = id => ({type: actions.BUG_RESOLVED, payload:{id}})
--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import {bugAdded , bugRemoved, bugResolved} from './actions'
-import store from './store'
+import {bugAdded , bugRemoved, bugResolved} from './store/bugs'
+import store from './store/store'
 
 console.log(store.getState());
 
 
-//  const unsubscribe = store.subscribe(()=>{
-//     console.log('Store Changed!!!');
-//     console.log('STORE',store.getState());
+ const unsubscribe = store.subscribe(()=>{
+    console.log('Store Changed!!!');
+    console.log('STORE',store.getState());
     
-// })
-// store.dispatch(bugAdded('Bug1'))
-// store.dispatch(bugAdded('Bug2'))
-// store.dispatch(bugResolved(2))
-// store.dispatch(bugRemoved(1))
-// store.dispatch(bugAdded('Bug3'))
-// unsubscribe()
+})
+store.dispatch(bugAdded('Bug1'))
+store.dispatch(bugAdded('Bug2'))
+store.dispatch(bugResolved(2))
+store.dispatch(bugRemoved(1))
+store.dispatch(bugAdded('Bug3'))
+unsubscribe()
